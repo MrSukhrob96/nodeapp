7bb8dd9e658e376a9ffa20efc754e7129c4a1b12
= <=>
2020-09-16 14:13:10 +0300

    new commit

--- main.js
+++ main.js
@@ -1,26 +1,22 @@
 'use strict';
 
-const http = require('http');
 const mysqlx = require('@mysql/xdevapi');
+const http = require('http');
 
-const port = process.env.PORT || 9999;
+const port = 9999;
+
 const statusOk = 200;
+const statusNotFaund = 404;
 const statusBadRequest = 400;
-const statusNotFound = 404;
 const statusInternalServerError = 500;
 const schema = 'social';
 
-const client = mysqlx.getClient({
-    user: 'app',
-    password: 'pass',
-    host: '0.0.0.0',
-    port: 33060
-});
 
-function sendResponse(response, {status = statusOk, headers = {}, body = null}) {
+function sendResponse(response, { status = statusOk, headers = {}, body = null }) {
     Object.entries(headers).forEach(function ([key, value]) {
         response.setHeader(key, value);
     });
+
     response.writeHead(status);
     response.end(body);
 }
@@ -35,90 +31,296 @@ function sendJSON(response, body) {
 }
 
 function map(columns) {
-    return row => row.reduce((res, value, i) => ({...res, [columns[i].getColumnLabel()]: value}), {});
+    return row => row.reduce((res, value, i) => ({ ...res, [columns[i].getColumnLabel()]: value }), {});
 }
+
 
 const methods = new Map();
 
-methods.set('/posts.get', async ({response, db}) => {
-    const table = await db.getTable('posts');
-    const result = await table.select(['id', 'content', 'likes', 'created'])
-        .where('removed != true')
-        .orderBy('id DESC')
-        .execute();
-
+methods.set('/posts.get', async ({ response, db }) => {
+    const table = db.getTable('posts');
+    const result = await table.select(['id', 'content', 'likes', 'created']).where('removed = :removed').bind('removed', false).orderBy('id DESC').execute();
     const data = result.fetchAll();
     const columns = result.getColumns();
     const posts = data.map(map(columns));
     sendJSON(response, posts);
 });
 
-methods.set('/posts.getById', async ({response, searchParams, db}) => {
-    if (!searchParams.has('id')) {
+
+methods.set('/posts.getById', async ({response, searchParams, db})=> {
+    if (!searchParams.has('id')){
         sendResponse(response, {status: statusBadRequest});
         return;
     }
 
     const id = Number(searchParams.get('id'));
-    if (Number.isNaN(id)) {
-        sendResponse(response, {status: statusBadRequest});
-        return;
+  
+    if (Number.isNaN(id)){
+      sendResponse(response, {status: statusBadRequest});
+      return;
     }
-
-    const table = await db.getTable('posts');
-    const result = await table.select(['id', 'content', 'likes', 'created'])
-        .where('id = :id AND removed != true')
-        .bind('id', id)
-        .execute();
-
+    const table = db.getTable('posts');
+    const result = await table.select(['id', 'content', 'likes', 'created']).where('id = :id and removed = false').bind('id', id).execute();
     const data = result.fetchAll();
-    const columns = result.getColumns();
-    const [post] = data.map(map(columns));
-
-    if (post === undefined) {
-        sendResponse(response, {status: statusNotFound});
+    if (data.length === 0) {
+        sendResponse(response, {status: statusNotFaund});
         return;
     }
+    const columns = result.getColumns();
+    const posts = data.map(map(columns));
+    sendJSON(response, posts[0]);
+  });
 
-    sendJSON(response, post);
-});
 
-methods.set('/posts.post', async ({response, searchParams, db}) => {
-    if (!searchParams.has('content')) {
+  methods.set('/posts.post', async ({response, searchParams, db})=> {
+    if (!searchParams.has('content')){
         sendResponse(response, {status: statusBadRequest});
         return;
     }
-
     const content = searchParams.get('content');
 
-    const table = await db.getTable('posts');
-    let result = await table.insert('content').values(content).execute();
+    const table = db.getTable('posts');
 
-    const id = result.getAutoIncrementValue();
-
-    result = await table.select(['id', 'content', 'likes', 'created'])
-        .where('id = :id')
-        .bind('id', id)
-        .execute();
-
-    const data = result.fetchAll();
-    const columns = result.getColumns();
-    const [post] = data.map(map(columns));
-
-    sendJSON(response, post);
-});
-
-const server = http.createServer(async (request, response) => {
-    const {pathname, searchParams} = new URL(request.url, `http://${request.headers.host}`);
-
-    const method = methods.get(pathname);
-    if (method === undefined) {
-        sendResponse(response, {status: statusNotFound});
+    const result = await table.insert('content').values(content).execute();
+    const removed = result.getAffectedItemsCount();
+    if (removed === 0) {
+        sendResponse(response, {status: statusNotFaund});
         return;
     }
+    const secondResult = await table.select(['id', 'content', 'likes', 'created']).where('content = :content').bind('content', content).execute();
+    const data = secondResult.fetchAll();
+
+    if (data.length === 0) {
+        sendResponse(response, {status: statusNotFaund});
+        return;
+    }
+    const columns = secondResult.getColumns();
+    const posts = data.map(map(columns));
+    sendJSON(response, posts[0]);
+  });
+  
+  methods.set('/posts.edit', async ({response, searchParams, db})=> {
+   
+    if (!searchParams.has('id')){
+        sendResponse(response, {status: statusBadRequest});
+        return;
+    }
+
+    const id = Number(searchParams.get('id'));
+    if (Number.isNaN(id)){
+      sendResponse(response, {status: statusBadRequest});
+      return;
+    }
+
+    if (!searchParams.has('content')){
+        sendResponse(response, {status: statusBadRequest});
+        return;
+    }
+    const content = searchParams.get('content');
+
+    const table = db.getTable('posts');
+    const result = await table.update().set('content', content).where('id = :id and removed=false').bind('id', id).execute();
+    const removed = result.getAffectedItemsCount();
+
+    if (removed === 0) {
+        sendResponse(response, {status: statusNotFaund});
+        return;
+    }
+
+    const secondResult = await table.select(['id', 'content', 'likes', 'created']).where('id = :id and removed = false').bind('id', id).execute();
+    const data = secondResult.fetchAll();
+
+    if (data.length === 0) {
+        sendResponse(response, {status: statusNotFaund});
+        return;
+    }
+
+    const columns = secondResult.getColumns();
+    const posts = data.map(map(columns));
+    sendJSON(response, posts[0]);
+  });
+
+
+  methods.set('/posts.delete', async ({response, searchParams, db})=> {
+    if (!searchParams.has('id')){
+        sendResponse(response, {status: statusBadRequest});
+        return;
+    }
+
+    const id = Number(searchParams.get('id'));
+  
+    if (Number.isNaN(id)){
+      sendResponse(response, {status: statusBadRequest});
+      return;
+    }
+    const table = db.getTable('posts');
+
+    const result = await table.update().set('removed', true).where('id = :id and removed=false').bind('id', id).execute();
+    const removed = result.getAffectedItemsCount();
+
+    if (removed === 0) {
+        sendResponse(response, {status: statusNotFaund});
+        return;
+    }
+
+    const secondResult = await table.select(['id', 'content', 'likes', 'created']).where('id = :id and removed = true').bind('id', id).execute();
+    const data = secondResult.fetchAll();
+    if (data.length === 0) {
+        sendResponse(response, {status: statusNotFaund});
+        return;
+    }
+    const columns = secondResult.getColumns();
+    const posts = data.map(map(columns));
+    sendJSON(response, posts[0]);
+  });
+
+
+  methods.set('/posts.restore', async ({response, searchParams, db})=> {
+    if (!searchParams.has('id')){
+        sendResponse(response, {status: statusBadRequest});
+        return;
+    }
+
+    const id = Number(searchParams.get('id'));
+  
+    if (Number.isNaN(id)){
+      sendResponse(response, {status: statusBadRequest});
+      return;
+    }
+    const table = db.getTable('posts');
+
+    const result = await table.update().set('removed', false).where('id = :id and removed=true').bind('id', id).execute();
+    const removed = result.getAffectedItemsCount();
+
+    if (removed === 0) {
+        sendResponse(response, {status: statusNotFaund});
+        return;
+    }
+
+    const secondResult = await table.select(['id', 'content', 'likes', 'created']).where('id = :id and removed = false').bind('id', id).execute();
+    const data = secondResult.fetchAll();
+    if (data.length === 0) {
+        sendResponse(response, {status: statusNotFaund});
+        return;
+    }
+    const columns = secondResult.getColumns();
+    const posts = data.map(map(columns));
+    sendJSON(response, posts[0]);
+  });
+
+
+  methods.set('/posts.like', async ({response, searchParams, db})=> {
+    if (!searchParams.has('id')){
+        sendResponse(response, {status: statusBadRequest});
+        return;
+    }
+
+    const id = Number(searchParams.get('id'));
+  
+    if (Number.isNaN(id)){
+      sendResponse(response, {status: statusBadRequest});
+      return;
+    }
+
+    
+    const table = db.getTable('posts');
+    
+    const resultLikesdata = await table.select(['likes']).where('id = :id and removed = false').bind('id', id).execute();
+    const dataLikesdata = resultLikesdata.fetchAll();
+    
+    if (dataLikesdata.length === 0) {
+        sendResponse(response, {status: statusNotFaund});
+        return;
+    }
+   
+    const addLike = dataLikesdata[0][0] + 1;
+
+    const result = await table.update().set('likes', addLike).where('id = :id and removed=false').bind('id', id).execute();
+    const removed = result.getAffectedItemsCount();
+
+    if (removed === 0) {
+        sendResponse(response, {status: statusNotFaund});
+        return;
+    }
+
+    const secondResult = await table.select(['id', 'content', 'likes', 'created']).where('id = :id and removed = false').bind('id', id).execute();
+    const data = secondResult.fetchAll();
+    if (data.length === 0) {
+        sendResponse(response, {status: statusNotFaund});
+        return;
+    }
+    const columns = secondResult.getColumns();
+    const posts = data.map(map(columns));
+    sendJSON(response, posts[0]);
+
+  });
+
+
+  methods.set('/posts.dislike', async ({response, searchParams, db})=> {
+    if (!searchParams.has('id')){
+        sendResponse(response, {status: statusBadRequest});
+        return;
+    }
+
+    const id = Number(searchParams.get('id'));
+  
+    if (Number.isNaN(id)){
+      sendResponse(response, {status: statusBadRequest});
+      return;
+    }
+
+    
+    const table = db.getTable('posts');
+    
+    const resultLikesdata = await table.select(['likes']).where('id = :id and removed = false').bind('id', id).execute();
+    const dataLikesdata = resultLikesdata.fetchAll();
+
+    if (dataLikesdata.length === 0) {
+        sendResponse(response, {status: statusNotFaund});
+        return;
+    }
+   
+    const addLike = dataLikesdata[0][0] - 1;
+
+    const result = await table.update().set('likes', addLike).where('id = :id and removed=false').bind('id', id).execute();
+    const removed = result.getAffectedItemsCount();
+
+    if (removed === 0) {
+        sendResponse(response, {status: statusNotFaund});
+        return;
+    }
+
+    const secondResult = await table.select(['id', 'content', 'likes', 'created']).where('id = :id and removed = false').bind('id', id).execute();
+    const data = secondResult.fetchAll();
+    if (data.length === 0) {
+        sendResponse(response, {status: statusNotFaund});
+        return;
+    }
+    const columns = secondResult.getColumns();
+    const posts = data.map(map(columns));
+    sendJSON(response, posts[0]);
+  });
+
+const server = http.createServer(async (request, response) => {
+
+    const { pathname, searchParams } = new URL(request.url, `http://${request.headers.host}`);
+    const method = methods.get(pathname);
+
+    if (method === undefined) {
+        sendResponse(response, { status: statusNotFaund });
+        return;
+    }
+
 
     let session = null;
     try {
+        const client = mysqlx.getClient({
+            user: 'app',
+            password: 'pass',
+            host: 'localhost',
+            port: '33060',
+        });
+
         session = await client.getSession();
         const db = await session.getSchema(schema);
 
@@ -131,9 +333,9 @@ const server = http.createServer(async (request, response) => {
         };
 
         await method(params);
+
     } catch (e) {
-        console.log(e);
-        sendResponse(response, {status: statusInternalServerError});
+        sendResponse(response, { status: statusInternalServerError });
     } finally {
         if (session !== null) {
             try {
@@ -143,6 +345,8 @@ const server = http.createServer(async (request, response) => {
             }
         }
     }
+
+
 });
 
 server.listen(port);
